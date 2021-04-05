cfa352247265b2d7f325ac68cc280908874b04f5
Mashud Alam <masud7in@yahoo.com>
2021-04-05 09:17:11 -0400

    My Project Setup Completed successfully

---
+++ shoppingbackend/src/main/java/com/toronto/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.toronto.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
